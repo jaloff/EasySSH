9ad4a677fdf5ffdbd2eb8ce7206fc4bf3fbd347c
jalov <picpwnz@gmail.com>
2018-01-16 20:27:51 +0100

    Allow to simple start/stop SSH

--- app/src/main/java/jalov/easyssh/MainActivity.java
+++ app/src/main/java/jalov/easyssh/MainActivity.java
@@ -1,12 +1,27 @@
 package jalov.easyssh;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+
+import static jalov.easyssh.RootManager.su;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String START_SSH = "start-ssh";
+    public static final String SSHD_APP_NAME = "/system/bin/sshd";
+    public static final String TAG = "MainActivity";
+    private Optional<ProcessInfo> sshdProcessInfo;
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +29,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(v -> toggleSSH());
+
+        updateSshdProcessInfo();
     }
 
     @Override
@@ -38,4 +58,53 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public void toggleSSH() {
+        if(sshdProcessInfo.isPresent()) {
+            su("kill -9 " + sshdProcessInfo.get().getPID());
+        } else {
+            su(START_SSH);
+        }
+
+        updateSshdProcessInfo();
+    }
+
+    public void updateSshdProcessInfo() {
+        TextView tv = (TextView) findViewById(R.id.tv_status);
+        sshdProcessInfo = getSshdProcessesInfo();
+        String status = "Stopped";
+        if(sshdProcessInfo.isPresent()) {
+            status = "Running";
+            fab.setImageResource(android.R.drawable.ic_media_pause);
+        } else {
+            fab.setImageResource(android.R.drawable.ic_media_play);
+        }
+        tv.setText(status);
+    }
+
+    public Optional<ProcessInfo> getSshdProcessesInfo() {
+        Optional<ProcessInfo> processInfo = Optional.empty();
+        try {
+            Process su = Runtime.getRuntime().exec("su");
+            DataOutputStream dos = new DataOutputStream(su.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(su.getInputStream()));
+
+            dos.writeBytes("ps | grep " + SSHD_APP_NAME + "\n");
+            dos.writeBytes("exit\n");
+            dos.flush();
+            su.waitFor();
+
+            if(reader.ready()) {
+                String line = reader.readLine();
+                String[] cols = line.split("\\s+");
+                processInfo = Optional.of(new ProcessInfo(cols[0], cols[1], cols[8]));
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return processInfo;
+    }
 }
---
+++ app/src/main/java/jalov/easyssh/ProcessInfo.java
@@ -0,0 +1,29 @@
+package jalov.easyssh;
+
+/**
+ * Created by jalov on 2018-01-16.
+ */
+
+public class ProcessInfo {
+    private String user;
+    private String PID;
+    private String name;
+
+    public ProcessInfo(String user, String PID, String name) {
+        this.user = user;
+        this.PID = PID;
+        this.name = name;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPID() {
+        return PID;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
